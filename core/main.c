4eebd10c3a44f49a9ef6c30f0fa7603735cc9485
KassemMostafa <mostafa.akassem03@gmail.com>
2023-05-05 19:58:51 +0200

    core done needs cleaning

--- core/main.c
+++ core/main.c
@@ -20,52 +20,24 @@ int FLAGS[6] = {0,0,0,0,0,0}; // {BEQ, BNE, BLE, BGE, BL, BG}
 int OVERFLOW = 0;
 int OVERFLOWLSH = 0;
 
-struct instruction decode(uint8_t *buffer) //Prend une instruction non PCC, découpe l'instruction et met chaque demi octet dans la bonne case de la structure instruction et puis appel execute
+struct instruction initInfo()
 {
     struct instruction info;
+    info.BCC = 0;
+    info.dest = 0;
+    info.IV = 0;
+    info.IV_Flag = 0;
+    info.offset = 0;
+    info.opCode = 0;
+    info.ope1 = 0;
+    info.ope2 = 0;
+    return info;
+}
+struct instruction decode(uint8_t *buffer) //Prend une instruction non PCC, découpe l'instruction et met chaque demi octet dans la bonne case de la structure instruction et puis appel execute
+{
+    struct instruction info = initInfo();
     uint32_t tempOffset = 0;
     int i;
-    
-
-    
-    // int line = 0x00184800;
-    // int line2 = 0xffffff;
-    // int line3 = 0x1000000;
-    // printf("Value of a: Hex: %X, Decimal: %d \n",line,line);
-    // printf("Value of a: Hex: %X, Decimal: %d \n",line2,line2);
-    // printf("Value of a: Hex: %X, Decimal: %d \n",line3,line3);
-    //All cases : read bit 31 to 0 == left to right Big Endian
-    // Case 1 : Not BCC 
-    //bit 31 to 28 => bcc => check 2.5.2 for values => 1 nibble => 4 bits
-    //bit 27 to  25 => always to 0 => + bit 24  == 1 nibble => reading bits 24 to 27 for 1 nibble
-    //bit 24  => Immediate value flag => Immediate Value flag
-    //bit 23 to 20 => opcode => 1 nibble => check table 2.5.4 for values
-    //bit 19 to 16 => ope1 => 1 nibble
-    //bit 15 to 12 => ope2 => 1 nibble
-    //bit 11 to 8 => dest => 1 nibble
-    //bit 7 to 0 => Immediate Value => 1 byte
-
-    
-
-    //Example writing right to left cause most significant is 31 all the way left
-    // MOV r1, r2
-    //1 : 00000000 => Immediate Value bits 0 to 7 reverse is same to have 7 to 0 => 0x00
-    //2 : 0001 =>  dest bytes 8 to 11 => 1 => 0001 =>   reverse 1000 => 0x8
-    //3 : 0010 => ope2 12 to 15 => 2 => 0010 => reverse 0100 => 0x4
-    //4 : 0001 => ope1 16 to 19 => 1 => 0001 => reverse 1000 => 0x8
-    //5 : 1000 => opcode 20 to 23 => 8 => 1000 => reverse 0001 => 0x1
-    //6 : 0 => immediate value flag + 000  bit 24 to 27 => reverse 0000 => 0x0
-    //7 : 0000 => bcc bit 28 to 31 => 0000 => 0x0
-    // Before reverse : left to right => bit 0 to 31 => 0000 0000 0001 0010 0001 1000 0000 0000
-    // After reverse : left to right => bit 31 to 0 => 0000 0000 0001 1000 0100 1000 0000 0000
-    // Expected Hex : 0x00184800 => correspond à after reverse :::
-                // 0x00 => 0x0 : bcc, 0x0 : IV flag buffer[0]
-                //0x18 => 0x1 : opcode, 0x8 : ope1 buffer[1]
-                //0x48 => 0x4 : ope2, Ox8 : dest buffer[2]
-                //0x00 => IV buffer[3]
-
-    //
-
     //Example writing right to left cause most significant is 31 all the way left
     // MOV r1, 2
     //1 : 00000010 => Immediate Value bits 0 to 7 reverse is 01000000 => 0x40
@@ -93,7 +65,7 @@ struct instruction decode(uint8_t *buffer) //Prend une instruction non PCC, déc
     if (buffer[0] == 0x00 || buffer[0] == 0x08) //no BCC , if à supprimer
     {
         printf("Buffer 0 => BCC|IV flag: %x|%x \n",buffer[0] & 0x10, buffer[0] & 0x08);
-        info.BCC = 0;
+        info.BCC = 0; //redundant
         info.IV_Flag = buffer[0] & 0x08;
         printf("Buffer 11 => opcode|ope1: %x, in decimal \n", buffer[1] >> 4 & 0xF);
         printf("Buffer 1 => opcode|ope1: %x|%x \n",buffer[1] >>4 & 0xF, buffer[1] & 0x0F);
@@ -130,6 +102,8 @@ struct instruction decode(uint8_t *buffer) //Prend une instruction non PCC, déc
         printf("offset without sign is %d \n", tempOffset);
         info.offset = tempOffset * info.offset;
         printf("offset is %d\n",info.offset);
+        info.offset *= 4; //pour avoir la valeur en nombre d'instructions
+        printf("offset is in bytes : %d\n",info.offset);
         
     }
     //Rappel bit shifting
@@ -221,14 +195,25 @@ void setFlag(int BEQ,int BNE,int BLE,int BGE,int BL,int BG)
 
 void checkFlag(uint64_t a, uint64_t b) //used when CMP
 {   
-    if (a == b)
-        setFlag(1,0,1,1,0,0);
+
     if (a > b)
         setFlag(0,1,0,1,0,1);
     if (a < b)
         setFlag(0,1,1,0,1,0);
-        
-    
+    if (a == b)
+        setFlag(1,0,1,1,0,0);        
+}
+
+int checkBCC(struct instruction info)
+{
+    switch(info.BCC)
+    {
+        case 8: //B
+            return 1;
+            break;
+        default:
+            return FLAGS[info.BCC-8];
+    }
 }
 
 void execute(struct instruction info)
@@ -349,72 +334,85 @@ void execute(struct instruction info)
     }
 }
 
-char* fetch(int PC, FILE *ptr) //BCC géré dans fetch with PC, lecture du binaire et gestion du PC selon si l'instruction est un BCC ou pas => SI BCC => calcul du nouveau BC et lecture de la bonne ligne selon l'offset, SI pas de BCC, envoi de toute la ligne à decode
+uint8_t* fetch(FILE *ptr) //BCC géré dans fetch with PC, lecture du binaire et gestion du PC selon si l'instruction est un BCC ou pas => SI BCC => calcul du nouveau BC et lecture de la bonne ligne selon l'offset, SI pas de BCC, envoi de toute la ligne à decode
 //Info à trouver/calculer dans fetch => BCC, offset, bloc d'instruction (sans decoder) et PC
 
 {
-    //TODO => handle multiple instructions
     size_t buffer_size = 4;
     uint8_t *buffer = malloc(buffer_size);
     size_t numBytes = 0;
-    //FILE *ptr;
+    int PC = 0;
     char bcc;
     struct instruction info;
     long filelen;
-    //printf("ok\n");
-    //ptr = fopen("binary.bin","rb");  // r for read, b for binary, open outside function
     //first call : I read 4 bytes, cursor at the end of the 4 bytes.
     fseek(ptr, 0, SEEK_END);          // Jump to the end of the file
-    filelen = ftell(ptr)/4;             // Get the current byte offset in the file
+    filelen = ftell(ptr);             // Get the current byte offset in the file
     printf("file length : %ld \n",filelen); //3 lines was to know the malloc size => //file len = number of bytes, 1 line of code = 4 bytes, line number = (filelen/PC)/4
     
     rewind(ptr); //called only at the beginning 
     buffer = malloc(buffer_size);
-    while (numBytes = fread(buffer, sizeof(uint8_t) , buffer_size, ptr) == buffer_size) //read until there's nothing left to read
-    //Buffersize = 4 => 32/4 => 8 => sizet is in bytes
+    while ((numBytes = fread(buffer, sizeof(uint8_t), buffer_size, ptr)) == buffer_size)
     {
-        printf("here");
-        
-        //fclose(ptr);
+        printf("PC after reading but before increment by = %d\n\n", PC);
+        printf("FILE LEN == %ld \n\n",filelen);
         for (int i = 0; i < 4; ++i)
-            printf("\n%x %d \n", buffer[i], i);
+            printf("%x %d \n", buffer[i], i);
         info = decode(buffer);
-        execute(info);
-        bcc = buffer[0] & 0x10;
-        printf("%x", bcc);
-        if (bcc == 0x0) //no BCC
+        PC += 4;
+        // execute(info);
+        // bcc = buffer[0] & 0x10;
+        // printf("%x", bcc);
+        if (info.BCC == 0) //no BCC
         {
-            //CALL EXECUTE
             printf("NO BCC\n");
-             //
-            PC++;
+            printf("File len == %ld\n", filelen);
+            printf("PC after execute at the end of the current line  %d\n\n",PC);
+            execute(info);
         }
         else
         {
         //todo 1- get offset, 2- verify if offset = max length of file or > 0, 3- fseek to the correct offset before calling again
         //can't get offset since compiler doesn't add it to binary correctly, skip for now
         //skip step 1 and assume the value is set:
-            if (info.offset + PC < 0 || info.offset + PC > filelen)
+            printf("BCC\n");
+            if (checkBCC(info))
             {
-                printf("Erreur de Segmentation");
-                exit(EXIT_FAILURE);
+                if (info.offset + PC - 4 < 0 || info.offset + PC > filelen)
+                {
+
+                //PC = 8 => ligne one is read and ligne 2 BCC, if offset = - 1 => -4 , fseek(PC+offset - 4)
+                //File len = 16, PC = 12 offset = 1 => 4
+                
+                    printf("File len == %ld\n", filelen);
+                    printf("offset is %d\n", info.offset);
+                    printf("PC is in bytes %d\n",PC);
+                    printf("Erreur de Segmentation\n");
+                    exit(EXIT_FAILURE);
+                }
+                else
+                {
+                    printf("File len == %ld\n", filelen);
+                    printf("offset is %d\n", info.offset);
+                    printf("PC is in bytes %d\n",PC);
+                    fseek(ptr,info.offset - 4,SEEK_CUR);
+                    PC += info.offset - 4;
+                }
             }
-            else
-            {
-                fseek(ptr,info.offset,SEEK_CUR);
-            }
+            
         }
+
     }    
-    return 0;
+    printf("PC outside = %d\n", PC);
+    return buffer;
 }
 
 
 
 
 void main(int argc, char *argv[]) {
-    char *buffer;
+    uint8_t *buffer;
     FILE *ptr;
-    int PC =1;
     int i;
     struct instruction info;
     
@@ -430,7 +428,7 @@ void main(int argc, char *argv[]) {
     overflowlshcheck(a,b);
     printf("Usage exemple : BIN_NAME <CODE> <STATE> (VERBOSE) \n");
     ptr = fopen("binary.bin","rb");
-    buffer = fetch(PC, ptr);
+    buffer = fetch(ptr);
     fclose(ptr);
     free(buffer);
 
